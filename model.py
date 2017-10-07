0f313557f7bba6ec67d6857a4771cc3b529f522c
Michael Tu <tuzhucheng@outlook.com>
2017-10-07 15:26:08 -0400

    Max pooling only

--- model.py
+++ model.py
@@ -39,14 +39,14 @@ class MPCNN(nn.Module):
         # compute number of inputs to first hidden layer
         COMP_1_COMPONENTS_HOLISTIC, COMP_1_COMPONENTS_PER_DIM, COMP_2_COMPONENTS = 2 + n_holistic_filters, 2 + n_word_dim, 2
         EXT_FEATS = 4 if ext_feats else 0
-        n_feat_h = 3 * len(self.filter_widths) * COMP_2_COMPONENTS
+        n_feat_h = len(self.filter_widths) * COMP_2_COMPONENTS
         n_feat_v = (
             # comparison units from holistic conv for min, max, mean pooling for non-infinite widths
-            3 * ((len(self.filter_widths) - 1) ** 2) * COMP_1_COMPONENTS_HOLISTIC +
+            ((len(self.filter_widths) - 1) ** 2) * COMP_1_COMPONENTS_HOLISTIC +
             # comparison units from holistic conv for min, max, mean pooling for infinite widths
-            3 * 3 +
+            3 +
             # comparison units from per-dim conv
-            2 * (len(self.filter_widths) - 1) * n_per_dim_filters * COMP_1_COMPONENTS_PER_DIM
+            (len(self.filter_widths) - 1) * n_per_dim_filters * COMP_1_COMPONENTS_PER_DIM
         )
         n_feat = n_feat_h + n_feat_v + EXT_FEATS
 
@@ -64,29 +64,24 @@ class MPCNN(nn.Module):
         for ws in self.filter_widths:
             if np.isinf(ws):
                 block_a[ws] = {
-                    'max': F.max_pool1d(sent.view(sent.size(0), 1, -1), sent.size(1) * sent.size(2)).view(sent.size(0), -1),
-                    'min': F.max_pool1d(-1 * sent.view(sent.size(0), 1, -1), sent.size(1) * sent.size(2)).view(sent.size(0), -1),
-                    'mean': F.avg_pool1d(sent.view(sent.size(0), 1, -1), sent.size(1) * sent.size(2)).view(sent.size(0), -1)
+                    'max': F.max_pool1d(sent.view(sent.size(0), 1, -1), sent.size(1) * sent.size(2)).view(sent.size(0), -1)
                 }
                 continue
 
             holistic_conv_out = self.holistic_conv_layers[ws - 1](sent)
             block_a[ws] = {
-                'max': F.max_pool1d(holistic_conv_out, holistic_conv_out.size(2)).view(-1, self.n_holistic_filters),
-                'min': F.max_pool1d(-1 * holistic_conv_out, holistic_conv_out.size(2)).view(-1, self.n_holistic_filters),
-                'mean': F.avg_pool1d(holistic_conv_out, holistic_conv_out.size(2)).view(-1, self.n_holistic_filters)
+                'max': F.max_pool1d(holistic_conv_out, holistic_conv_out.size(2)).view(-1, self.n_holistic_filters)
             }
 
             per_dim_conv_out = self.per_dim_conv_layers[ws - 1](sent)
             block_b[ws] = {
-                'max': F.max_pool1d(per_dim_conv_out, per_dim_conv_out.size(2)).view(-1, self.n_word_dim, self.n_per_dim_filters),
-                'min': F.max_pool1d(-1 * per_dim_conv_out, per_dim_conv_out.size(2)).view(-1, self.n_word_dim, self.n_per_dim_filters)
+                'max': F.max_pool1d(per_dim_conv_out, per_dim_conv_out.size(2)).view(-1, self.n_word_dim, self.n_per_dim_filters)
             }
         return block_a, block_b
 
     def _algo_1_horiz_comp(self, sent1_block_a, sent2_block_a):
         comparison_feats = []
-        for pool in ('max', 'min', 'mean'):
+        for pool in ('max', ):
             for ws in self.filter_widths:
                 x1 = sent1_block_a[ws][pool]
                 x2 = sent2_block_a[ws][pool]
@@ -98,7 +93,7 @@ class MPCNN(nn.Module):
     def _algo_2_vert_comp(self, sent1_block_a, sent2_block_a, sent1_block_b, sent2_block_b):
         comparison_feats = []
         ws_no_inf = [w for w in self.filter_widths if not np.isinf(w)]
-        for pool in ('max', 'min', 'mean'):
+        for pool in ('max', ):
             for ws1 in self.filter_widths:
                 x1 = sent1_block_a[ws1][pool]
                 batch_size = x1.size()[0]
@@ -109,7 +104,7 @@ class MPCNN(nn.Module):
                         comparison_feats.append(F.pairwise_distance(x1, x2))
                         comparison_feats.append(torch.abs(x1 - x2))
 
-        for pool in ('max', 'min'):
+        for pool in ('max', ):
             for ws in ws_no_inf:
                 oG_1B = sent1_block_b[ws][pool]
                 oG_2B = sent2_block_b[ws][pool]

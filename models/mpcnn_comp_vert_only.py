3d9c2bfbec5b485b4536ce70f179600bfe2cd5ad
Michael Tu <tuzhucheng@outlook.com>
2018-03-12 11:46:50 -0400

    MPCNN vertical comparison only variant

--- models/mpcnn_comp_vert_only.py
+++ models/mpcnn_comp_vert_only.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class MPCNN(nn.Module):
+class MPCNNCompVertOnly(nn.Module):
 
     def __init__(self, n_word_dim, n_holistic_filters, n_per_dim_filters, filter_widths, hidden_layer_units, num_classes, dropout, ext_feats, attention):
-        super(MPCNN, self).__init__()
-        self.arch = 'mpcnn'
+        super(MPCNNCompVertOnly, self).__init__()
+        self.arch = 'mpcnn_comp_vert_only'
         self.n_word_dim = n_word_dim
         self.n_holistic_filters = n_holistic_filters
         self.n_per_dim_filters = n_per_dim_filters
@@ -40,7 +40,6 @@ class MPCNN(nn.Module):
         # compute number of inputs to first hidden layer
         COMP_1_COMPONENTS_HOLISTIC, COMP_1_COMPONENTS_PER_DIM, COMP_2_COMPONENTS = 2 + n_holistic_filters, 2 + self.in_channels, 2
         EXT_FEATS = 4 if ext_feats else 0
-        n_feat_h = 3 * len(self.filter_widths) * COMP_2_COMPONENTS
         n_feat_v = (
             # comparison units from holistic conv for min, max, mean pooling for non-infinite widths
             3 * ((len(self.filter_widths) - 1) ** 2) * COMP_1_COMPONENTS_HOLISTIC +
@@ -49,7 +48,7 @@ class MPCNN(nn.Module):
             # comparison units from per-dim conv
             2 * (len(self.filter_widths) - 1) * n_per_dim_filters * COMP_1_COMPONENTS_PER_DIM
         )
-        n_feat = n_feat_h + n_feat_v + EXT_FEATS
+        n_feat = n_feat_v + EXT_FEATS
 
         self.final_layers = nn.Sequential(
             nn.Linear(n_feat, hidden_layer_units),
@@ -102,17 +101,6 @@ class MPCNN(nn.Module):
             }
         return block_a, block_b
 
-    def _algo_1_horiz_comp(self, sent1_block_a, sent2_block_a):
-        comparison_feats = []
-        for pool in ('max', 'min', 'mean'):
-            for ws in self.filter_widths:
-                x1 = sent1_block_a[ws][pool]
-                x2 = sent2_block_a[ws][pool]
-                batch_size = x1.size()[0]
-                comparison_feats.append(F.cosine_similarity(x1, x2).contiguous().view(batch_size, 1))
-                comparison_feats.append(F.pairwise_distance(x1, x2))
-        return torch.cat(comparison_feats, dim=1)
-
     def _algo_2_vert_comp(self, sent1_block_a, sent2_block_a, sent1_block_b, sent2_block_b):
         comparison_feats = []
         ws_no_inf = [w for w in self.filter_widths if not np.isinf(w)]
@@ -151,9 +139,8 @@ class MPCNN(nn.Module):
         sent2_block_a, sent2_block_b = self._get_blocks_for_sentence(sent2)
 
         # Similarity measurement layer
-        feat_h = self._algo_1_horiz_comp(sent1_block_a, sent2_block_a)
         feat_v = self._algo_2_vert_comp(sent1_block_a, sent2_block_a, sent1_block_b, sent2_block_b)
-        combined_feats = [feat_h, feat_v, ext_feats] if self.ext_feats else [feat_h, feat_v]
+        combined_feats = [feat_v, ext_feats] if self.ext_feats else [feat_v]
         feat_all = torch.cat(combined_feats, dim=1)
 
         preds = self.final_layers(feat_all)

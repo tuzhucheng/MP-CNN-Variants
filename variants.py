e18a619c803286f6d8bb4c7f1dbeda9bff541501
Michael Tu <tuzhucheng@outlook.com>
2018-03-17 02:39:24 -0400

    Max pool + holistic filter only variant

--- variants.py
+++ variants.py
@@ -12,6 +12,7 @@ from models.mpcnn_comp_horiz_only import MPCNNCompHorizOnly
 from models.mpcnn_comp_vert_only import MPCNNCompVertOnly
 from models.mpcnn_comp_unit1_only import MPCNNCompUnit1Only
 from models.mpcnn_comp_unit2_only import MPCNNCompUnit2Only
+from models.mpcnn_holistic_pool_max_only import MPCNNHolisticPoolMaxOnly
 
 import numpy as np
 
@@ -30,7 +31,8 @@ class VariantFactory(object):
                 'mpcnn_comp_horiz_only': MPCNNCompHorizOnly,
                 'mpcnn_comp_vert_only': MPCNNCompVertOnly,
                 'mpcnn_comp_unit1_only': MPCNNCompUnit1Only,
-                'mpcnn_comp_unit2_only': MPCNNCompUnit2Only
+                'mpcnn_comp_unit2_only': MPCNNCompUnit2Only,
+                'mpcnn_holistic_pool_max_only': MPCNNHolisticPoolMaxOnly
             }
 
             filter_widths = list(range(1, args.max_window_size + 1)) + [np.inf]

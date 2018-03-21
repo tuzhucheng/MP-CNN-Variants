fd2fe10f987b4d840723c5f29df4630bc5b35243
Michael Tu <tuzhucheng@outlook.com>
2018-03-20 21:41:35 -0400

    inherit comp* from variant

--- models/mpcnn_comp_unit2_only.py
+++ models/mpcnn_comp_unit2_only.py
@@ -3,11 +3,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from models.mpcnn_variant_base import MPCNNVariantBase
 
-class MPCNNCompUnit2Only(nn.Module):
+
+class MPCNNCompUnit2Only(MPCNNVariantBase):
 
     def __init__(self, n_word_dim, n_holistic_filters, n_per_dim_filters, filter_widths, hidden_layer_units, num_classes, dropout, ext_feats, attention):
-        super(MPCNNCompUnit2Only, self).__init__()
+        super(MPCNNCompUnit2Only, self).__init__(n_word_dim, n_holistic_filters, n_per_dim_filters, filter_widths, hidden_layer_units, num_classes, dropout, ext_feats, attention)
         self.arch = 'mpcnn_comp_unit2_only'
         self.n_word_dim = n_word_dim
         self.n_holistic_filters = n_holistic_filters
@@ -18,7 +20,7 @@ class MPCNNCompUnit2Only(nn.Module):
         holistic_conv_layers = []
         per_dim_conv_layers = []
 
-        self.in_channels = n_word_dim if not attention else 2*n_word_dim
+        self.in_channels = n_word_dim if attention == 'none' else 2*n_word_dim
 
         for ws in filter_widths:
             if np.isinf(ws):
@@ -58,22 +60,6 @@ class MPCNNCompUnit2Only(nn.Module):
             nn.Linear(hidden_layer_units, num_classes),
             nn.LogSoftmax(1)
         )
-
-    def concat_attention(self, sent1, sent2):
-        sent1_transposed = sent1.transpose(1, 2)
-        attention_dot = torch.bmm(sent1_transposed, sent2)
-        sent1_norms = torch.norm(sent1_transposed, p=2, dim=2, keepdim=True)
-        sent2_norms = torch.norm(sent2, p=2, dim=1, keepdim=True)
-        attention_norms = torch.bmm(sent1_norms, sent2_norms)
-        attention_matrix = attention_dot / attention_norms
-
-        attention_weight_vec1 = F.softmax(attention_matrix.sum(2), 1)
-        attention_weight_vec2 = F.softmax(attention_matrix.sum(1), 1)
-        attention_weighted_sent1 = attention_weight_vec1.unsqueeze(1).expand(-1, self.n_word_dim, -1) * sent1
-        attention_weighted_sent2 = attention_weight_vec2.unsqueeze(1).expand(-1, self.n_word_dim, -1) * sent2
-        attention_emb1 = torch.cat((attention_weighted_sent1, sent1), dim=1)
-        attention_emb2 = torch.cat((attention_weighted_sent2, sent2), dim=1)
-        return attention_emb1, attention_emb2
 
     def _get_blocks_for_sentence(self, sent):
         block_a = {}
@@ -139,10 +125,10 @@ class MPCNNCompUnit2Only(nn.Module):
 
         return torch.cat(comparison_feats, dim=1)
 
-    def forward(self, sent1, sent2, ext_feats=None):
+    def forward(self, sent1, sent2, ext_feats=None, word_to_doc_count=None, raw_sent1=None, raw_sent2=None):
         # Attention
-        if self.attention:
-            sent1, sent2 = self.concat_attention(sent1, sent2)
+        if self.attention != 'none':
+            sent1, sent2 = self.concat_attention(sent1, sent2, word_to_doc_count, raw_sent1, raw_sent2)
 
         # Sentence modeling module
         sent1_block_a, sent1_block_b = self._get_blocks_for_sentence(sent1)

abac22f76cd4433aa4f3505c154be106e6c1abee
Michael Tu <tuzhucheng@outlook.com>
2018-04-24 22:04:28 -0400

    Horizontal comparison - cosine and euclidean comparison on different filter indices

--- models/mpcnn.py
+++ models/mpcnn.py
@@ -65,7 +65,7 @@ class MPCNN(MPCNNVariantBase):
 
         # compute number of inputs to first hidden layer
         COMP_1_COMPONENTS_HOLISTIC, COMP_1_COMPONENTS_PER_DIM, COMP_2_COMPONENTS = 2 + n_holistic_filters, 2 + self.in_channels, 2
-        n_feat_h = 3 * len(self.filter_widths) * COMP_2_COMPONENTS
+        n_feat_h = 3 * n_holistic_filters * COMP_2_COMPONENTS
         n_feat_v = (
             # comparison units from holistic conv for min, max, mean pooling for non-infinite widths
             3 * ((len(self.filter_widths) - 1) ** 2) * COMP_1_COMPONENTS_HOLISTIC +
@@ -117,12 +117,27 @@ class MPCNN(MPCNNVariantBase):
     def _algo_1_horiz_comp(self, sent1_block_a, sent2_block_a):
         comparison_feats = []
         for pool in ('max', 'min', 'mean'):
+            regM1, regM2 = [], []
             for ws in self.filter_widths:
-                x1 = sent1_block_a[ws][pool]
-                x2 = sent2_block_a[ws][pool]
-                batch_size = x1.size()[0]
-                comparison_feats.append(F.cosine_similarity(x1, x2).contiguous().view(batch_size, 1))
-                comparison_feats.append(F.pairwise_distance(x1, x2))
+                x1 = sent1_block_a[ws][pool].unsqueeze(2)
+                x2 = sent2_block_a[ws][pool].unsqueeze(2)
+                if np.isinf(ws):
+                    x1 = x1.expand(-1, self.n_holistic_filters, -1)
+                    x2 = x2.expand(-1, self.n_holistic_filters, -1)
+                regM1.append(x1)
+                regM2.append(x2)
+
+            regM1 = torch.cat(regM1, dim=2)
+            regM2 = torch.cat(regM2, dim=2)
+
+            comparison_feats.append(F.cosine_similarity(regM1, regM2, dim=2))
+
+            pairwise_distances = []
+            for x1, x2 in zip(regM1, regM2):
+                dist = F.pairwise_distance(x1, x2).view(1, -1)
+                pairwise_distances.append(dist)
+
+            comparison_feats.append(torch.cat(pairwise_distances))
         return torch.cat(comparison_feats, dim=1)
 
     def _algo_2_vert_comp(self, sent1_block_a, sent2_block_a, sent1_block_b, sent2_block_b):
@@ -131,7 +146,7 @@ class MPCNN(MPCNNVariantBase):
         for pool in ('max', 'min', 'mean'):
             for ws1 in self.filter_widths:
                 x1 = sent1_block_a[ws1][pool]
-                batch_size = x1.size()[0]
+                batch_size = x1.size(0)
                 for ws2 in self.filter_widths:
                     x2 = sent2_block_a[ws2][pool]
                     if (not np.isinf(ws1) and not np.isinf(ws2)) or (np.isinf(ws1) and np.isinf(ws2)):
@@ -146,7 +161,7 @@ class MPCNN(MPCNNVariantBase):
                 for i in range(0, self.n_per_dim_filters):
                     x1 = oG_1B[:, :, i]
                     x2 = oG_2B[:, :, i]
-                    batch_size = x1.size()[0]
+                    batch_size = x1.size(0)
                     comparison_feats.append(F.cosine_similarity(x1, x2).contiguous().view(batch_size, 1))
                     comparison_feats.append(F.pairwise_distance(x1, x2))
                     comparison_feats.append(torch.abs(x1 - x2))

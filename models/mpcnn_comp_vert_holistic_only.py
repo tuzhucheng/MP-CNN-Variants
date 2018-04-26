62b0d907abb4d8dd04857baf958e3fca5e3f265f
Michael Tu <tuzhucheng@outlook.com>
2018-04-26 18:35:59 -0400

    MPCNN Comp Horiz/Vert Architectures use OOP and Refactored to PyTorch 0.4.0

--- models/mpcnn_comp_vert_holistic_only.py
+++ models/mpcnn_comp_vert_holistic_only.py
@@ -36,22 +36,26 @@ class MPCNNCompVertHolisticOnly(MPCNNVariantBase):
         self.holistic_conv_layers = nn.ModuleList(holistic_conv_layers)
 
         # compute number of inputs to first hidden layer
-        COMP_1_COMPONENTS_HOLISTIC, COMP_2_COMPONENTS = 2 + n_holistic_filters, 2
-        n_feat_v = (
-            # comparison units from holistic conv for min, max, mean pooling for non-infinite widths
-            3 * ((len(self.filter_widths) - 1) ** 2) * COMP_1_COMPONENTS_HOLISTIC +
-            # comparison units from holistic conv for min, max, mean pooling for infinite widths
-            3 * 3
-        )
-        n_feat = n_feat_v + ext_feats
+        n_feats = self._get_n_feats()
 
         self.final_layers = nn.Sequential(
-            nn.Linear(n_feat, hidden_layer_units),
+            nn.Linear(n_feats, hidden_layer_units),
             nn.Tanh(),
             nn.Dropout(dropout),
             nn.Linear(hidden_layer_units, num_classes),
             nn.LogSoftmax(1)
         )
+
+    def _get_n_feats(self):
+        COMP_1_COMPONENTS_HOLISTIC, COMP_2_COMPONENTS = 2 + self.n_holistic_filters, 2
+        n_feats_v = (
+            # comparison units from holistic conv for min, max, mean pooling for non-infinite widths
+            3 * ((len(self.filter_widths) - 1) ** 2) * COMP_1_COMPONENTS_HOLISTIC +
+            # comparison units from holistic conv for min, max, mean pooling for infinite widths
+            3 * 3
+        )
+        n_feats = n_feats_v + self.ext_feats
+        return n_feats
 
     def _get_blocks_for_sentence(self, sent):
         block_a = {}
@@ -75,16 +79,14 @@ class MPCNNCompVertHolisticOnly(MPCNNVariantBase):
 
     def _algo_2_vert_comp(self, sent1_block_a, sent2_block_a):
         comparison_feats = []
-        ws_no_inf = [w for w in self.filter_widths if not np.isinf(w)]
         for pool in ('max', 'min', 'mean'):
             for ws1 in self.filter_widths:
                 x1 = sent1_block_a[ws1][pool]
-                batch_size = x1.size()[0]
                 for ws2 in self.filter_widths:
                     x2 = sent2_block_a[ws2][pool]
                     if (not np.isinf(ws1) and not np.isinf(ws2)) or (np.isinf(ws1) and np.isinf(ws2)):
-                        comparison_feats.append(F.cosine_similarity(x1, x2).contiguous().view(batch_size, 1))
-                        comparison_feats.append(F.pairwise_distance(x1, x2))
+                        comparison_feats.append(F.cosine_similarity(x1, x2).unsqueeze(1))
+                        comparison_feats.append(F.pairwise_distance(x1, x2).unsqueeze(1))
                         comparison_feats.append(torch.abs(x1 - x2))
 
         return torch.cat(comparison_feats, dim=1)

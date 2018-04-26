5043b3072870807af6d39a6e81c519c9fa0058cf
Michael Tu <tuzhucheng@outlook.com>
2018-04-26 19:07:33 -0400

    MPCNN Holistic Architectures use OOP and Refactored to PyTorch 0.4.0

--- models/mpcnn_holistic_only.py
+++ models/mpcnn_holistic_only.py
@@ -35,23 +35,27 @@ class MPCNNHolisticOnly(MPCNNVariantBase):
         self.holistic_conv_layers = nn.ModuleList(holistic_conv_layers)
 
         # compute number of inputs to first hidden layer
-        COMP_1_COMPONENTS_HOLISTIC, COMP_1_COMPONENTS_PER_DIM, COMP_2_COMPONENTS = 2 + n_holistic_filters, 2 + self.in_channels, 2
-        n_feat_h = 3 * len(self.filter_widths) * COMP_2_COMPONENTS
-        n_feat_v = (
-            # comparison units from holistic conv for min, max, mean pooling for non-infinite widths
-            3 * ((len(self.filter_widths) - 1) ** 2) * COMP_1_COMPONENTS_HOLISTIC +
-            # comparison units from holistic conv for min, max, mean pooling for infinite widths
-            3 * 3
-        )
-        n_feat = n_feat_h + n_feat_v + ext_feats
+        n_feats = self._get_n_feats()
 
         self.final_layers = nn.Sequential(
-            nn.Linear(n_feat, hidden_layer_units),
+            nn.Linear(n_feats, hidden_layer_units),
             nn.Tanh(),
             nn.Dropout(dropout),
             nn.Linear(hidden_layer_units, num_classes),
             nn.LogSoftmax(1)
         )
+
+    def _get_n_feats(self):
+        COMP_1_COMPONENTS_HOLISTIC, COMP_1_COMPONENTS_PER_DIM, COMP_2_COMPONENTS = 2 + self.n_holistic_filters, 2 + self.in_channels, 2
+        n_feats_h = 3 * len(self.filter_widths) * COMP_2_COMPONENTS
+        n_feats_v = (
+            # comparison units from holistic conv for min, max, mean pooling for non-infinite widths
+            3 * ((len(self.filter_widths) - 1) ** 2) * COMP_1_COMPONENTS_HOLISTIC +
+            # comparison units from holistic conv for min, max, mean pooling for infinite widths
+            3 * 3
+        )
+        n_feats = n_feats_h + n_feats_v + self.ext_feats
+        return n_feats
 
     def _get_blocks_for_sentence(self, sent):
         block_a = {}
@@ -80,31 +84,28 @@ class MPCNNHolisticOnly(MPCNNVariantBase):
             for ws in self.filter_widths:
                 x1 = sent1_block_a[ws][pool]
                 x2 = sent2_block_a[ws][pool]
-                batch_size = x1.size()[0]
-                comparison_feats.append(F.cosine_similarity(x1, x2).contiguous().view(batch_size, 1))
+                comparison_feats.append(F.cosine_similarity(x1, x2))
                 comparison_feats.append(F.pairwise_distance(x1, x2))
-        return torch.cat(comparison_feats, dim=1)
+        return torch.stack(comparison_feats, dim=1)
 
     def _algo_2_vert_comp(self, sent1_block_a, sent2_block_a):
         comparison_feats = []
-        ws_no_inf = [w for w in self.filter_widths if not np.isinf(w)]
         for pool in ('max', 'min', 'mean'):
             for ws1 in self.filter_widths:
                 x1 = sent1_block_a[ws1][pool]
-                batch_size = x1.size()[0]
                 for ws2 in self.filter_widths:
                     x2 = sent2_block_a[ws2][pool]
                     if (not np.isinf(ws1) and not np.isinf(ws2)) or (np.isinf(ws1) and np.isinf(ws2)):
-                        comparison_feats.append(F.cosine_similarity(x1, x2).contiguous().view(batch_size, 1))
-                        comparison_feats.append(F.pairwise_distance(x1, x2))
+                        comparison_feats.append(F.cosine_similarity(x1, x2).unsqueeze(1))
+                        comparison_feats.append(F.pairwise_distance(x1, x2).unsqueeze(1))
                         comparison_feats.append(torch.abs(x1 - x2))
 
         return torch.cat(comparison_feats, dim=1)
 
-    def forward(self, sent1, sent2, ext_feats=None, word_to_doc_count=None, raw_sent1=None, raw_sent2=None):
+    def forward(self, sent1, sent2, ext_feats=None, word_to_doc_count=None, raw_sent1=None, raw_sent2=None, sent1_nonstatic=None, sent2_nonstatic=None):
         # Attention
         if self.attention != 'none':
-            sent1, sent2 = self.concat_attention(sent1, sent2, word_to_doc_count, raw_sent1, raw_sent2, sent1_nonstatic=None, sent2_nonstatic=None)
+            sent1, sent2 = self.concat_attention(sent1, sent2, word_to_doc_count, raw_sent1, raw_sent2)
 
         # Sentence modeling module
         sent1_block_a = self._get_blocks_for_sentence(sent1)

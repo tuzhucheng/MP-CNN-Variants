1e613e05945a6b68536f543fcc59082934b1dad6
Michael Tu <tuzhucheng@outlook.com>
2018-04-29 16:06:16 -0400

    Variants should use independent filters for max, min, mean pooling

--- models/mpcnn_single_window_with_inf.py
+++ models/mpcnn_single_window_with_inf.py
@@ -1,6 +1,4 @@
-import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
 
 from models.mpcnn import MPCNN
 
@@ -15,12 +13,27 @@ class MPCNNSingleWindowWithInf(MPCNN):
     def _add_layers(self):
         padding = self.filter_widths[0] - 1 if self.wide_conv else 0
 
-        self.holistic_conv_layer = nn.Sequential(
+        self.holistic_conv_layer_max = nn.Sequential(
             nn.Conv1d(self.in_channels, self.n_holistic_filters, self.filter_widths[0], padding=padding),
             nn.Tanh()
         )
 
-        self.per_dim_conv_layer = nn.Sequential(
+        self.holistic_conv_layer_min = nn.Sequential(
+            nn.Conv1d(self.in_channels, self.n_holistic_filters, self.filter_widths[0], padding=padding),
+            nn.Tanh()
+        )
+
+        self.holistic_conv_layer_mean = nn.Sequential(
+            nn.Conv1d(self.in_channels, self.n_holistic_filters, self.filter_widths[0], padding=padding),
+            nn.Tanh()
+        )
+
+        self.per_dim_conv_layer_max = nn.Sequential(
+            nn.Conv1d(self.in_channels, self.in_channels * self.n_per_dim_filters, self.filter_widths[0], padding=padding, groups=self.in_channels),
+            nn.Tanh()
+        )
+
+        self.per_dim_conv_layer_min = nn.Sequential(
             nn.Conv1d(self.in_channels, self.in_channels * self.n_per_dim_filters, self.filter_widths[0], padding=padding, groups=self.in_channels),
             nn.Tanh()
         )
@@ -38,30 +51,3 @@ class MPCNNSingleWindowWithInf(MPCNN):
         )
         n_feats = n_feats_h + n_feats_v + self.ext_feats
         return n_feats
-
-    def _get_blocks_for_sentence(self, sent):
-        block_a = {}
-        block_b = {}
-        for ws in self.filter_widths:
-            if np.isinf(ws):
-                sent_flattened, sent_flattened_size = sent.contiguous().view(sent.size(0), 1, -1), sent.size(1) * sent.size(2)
-                block_a[ws] = {
-                    'max': F.max_pool1d(sent_flattened, sent_flattened_size).view(sent.size(0), -1),
-                    'min': F.max_pool1d(-1 * sent_flattened, sent_flattened_size).view(sent.size(0), -1),
-                    'mean': F.avg_pool1d(sent_flattened, sent_flattened_size).view(sent.size(0), -1)
-                }
-                continue
-
-            holistic_conv_out = self.holistic_conv_layer(sent)
-            block_a[ws] = {
-                'max': F.max_pool1d(holistic_conv_out, holistic_conv_out.size(2)).contiguous().view(-1, self.n_holistic_filters),
-                'min': F.max_pool1d(-1 * holistic_conv_out, holistic_conv_out.size(2)).contiguous().view(-1, self.n_holistic_filters),
-                'mean': F.avg_pool1d(holistic_conv_out, holistic_conv_out.size(2)).contiguous().view(-1, self.n_holistic_filters)
-            }
-
-            per_dim_conv_out = self.per_dim_conv_layer(sent)
-            block_b[ws] = {
-                'max': F.max_pool1d(per_dim_conv_out, per_dim_conv_out.size(2)).contiguous().view(-1, self.in_channels, self.n_per_dim_filters),
-                'min': F.max_pool1d(-1 * per_dim_conv_out, per_dim_conv_out.size(2)).contiguous().view(-1, self.in_channels, self.n_per_dim_filters)
-            }
-        return block_a, block_b
